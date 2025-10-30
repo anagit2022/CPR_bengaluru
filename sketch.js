0379b6d339bf4125b7257fc7bdd443b69559fead
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-10-30 18:03:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -334,6 +334,7 @@ function draw(){
   text("width/2 : " + width ,120,40);
   text(" height/2 : " + height ,200,40);
   text("g: "+genderState ,350,40);
+   text("s: "+currentState ,350,80);
   //______________________________________
   
 }
