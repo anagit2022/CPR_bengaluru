82342a11ccd2f76ff638a6a96e2312643f541b68
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-11-01 00:42:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -454,7 +454,7 @@ function mousePressed(){
       mouseY < noy + noh
     ) {
       currentState = "cprD_yes";
-      gasp_aud.play();
+      normal_breath_aud.play();
       console.log(currentState);
       }
   }else if(currentState == "cprD_yes"){
