05ac505e028841fa1acf685798acc57321283be5
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-11-02 17:25:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -181,6 +181,11 @@ function setup(){
   call8y = windowHeight*0.6; 
   call8w =60 ;
   call8h =60 ;
+  / call 2
+  call2x =windowWidth*0.45;  
+  call2y = windowHeight*0.43; 
+  call2w =60 ;
+  call2h =60 ;
   // call ring
   callx =windowWidth*0.4;  
   cally = windowHeight*0.8; 
